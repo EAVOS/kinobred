84f0c5a65ce34ca3c5d4e8ab006eb8c1ab57cf31
EAVOS <50738262+EAVOS@users.noreply.github.com>
2026-04-29 17:24:57 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.KinoBredConfig = {
-    GAS_URL: 'https://script.google.com/macros/s/AKfycbxRjeQRHmdrEecnSMIgi9J1SsVuTFn42rF3cyUKdo2lmx3MLRIX97jbwe62F0WUDXOH0g/exec',
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbyWsJFoUX1Pltnt5GMNGYdlcqxfYuj9-z5wD16f7RrNFOjR11LnSqIFyyUUQCyz84pOMg/exec',
     BOT_USERNAME: 'KinoBredBot',
-    VERSION: '1.0.15'
+    VERSION: '1.0.18'
 };
