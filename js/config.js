92eaa8a4da10cca9f346a756ca77b58a2194b198
EAVOS <50738262+EAVOS@users.noreply.github.com>
2026-04-29 10:03:22 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,11 @@
+// Конфигурация КиноБреда
+window.KinoBredConfig = {
+    // URL Google Apps Script (заменить на реальный после деплоя)
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbwbDEN3VQqL-hFVsAm3R4kFDFfjZmHYiZMys5mefSm9FewRqn0SaXH7jreJja7NZtyPew/exec',
+    
+    // Настройки бота
+    BOT_USERNAME: 'KinoBredBot',
+    
+    // Версия для сброса кэша
+    VERSION: '1.0.0'
+};
